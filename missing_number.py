428714809a32ef45900f3af5b3ec1ab732378665
Udaya Kumar Vamaraju <122080642+UdayaKumar-Vamaraju@users.noreply.github.com>
2025-03-12 16:36:21 +0530

    Update missing_number.py

--- missing_number.py
+++ missing_number.py
@@ -10,3 +10,20 @@ If the array contains the numbers: [1, 2, 3, 5, 6]
 Then the missing number is 4.
 
 """
+
+n=int(input("Enter the size of an array:"))
+
+arr=[]
+print("Enter the numbers:")
+for i in range(0,n-1):
+    arr.append(int(input()))
+
+expected_sum = n * (n + 1) // 2
+
+actual_sum=sum(arr)
+
+missing_num = expected_sum - actual_sum
+
+print("The  missing number is:",missing_num)
+
+    
