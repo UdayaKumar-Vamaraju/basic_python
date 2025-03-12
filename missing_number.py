a7364d2751a9d11787318f60d00b3e55aad2bf66
Udaya Kumar Vamaraju <122080642+UdayaKumar-Vamaraju@users.noreply.github.com>
2025-03-12 16:38:03 +0530

    Create missing_number.py

---
+++ missing_number.py
@@ -0,0 +1,26 @@
+"""
+Problem Explanation:
+
+You are given an array that contains n-1 distinct numbers. These numbers are chosen from the range 1 to n, where 
+n is the total number of elements you expect. Since there is one missing number, the array contains every number 
+from the range except one.
+
+For example, if n = 6, the complete set of numbers would be: [1, 2, 3, 4, 5, 6]
+If the array contains the numbers: [1, 2, 3, 5, 6]
+Then the missing number is 4.
+
+"""
+n=int(input("Enter the size of an array:"))
+
+arr=[]
+print("Enter the numbers:")
+for i in range(0,n-1):
+    arr.append(int(input()))
+
+expected_sum = n * (n + 1) // 2
+
+actual_sum=sum(arr)
+
+missing_num = expected_sum - actual_sum
+
+print("The  missing number is:",missing_num)
